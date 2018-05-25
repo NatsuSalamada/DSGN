15f3a9118e6323223f1263f4a19cbb048ed1f0f6
eciscbss <eciscbs@gmail.com>
2018-05-25 17:32:56 +0700

    Cell

--- DSGN/MVC/View/GioiThieuView.swift
+++ DSGN/MVC/View/GioiThieuView.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
-class GioiThieuView: UIViewController {
+class GioiThieuView: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 21
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCellIntro
+        return cell
+    }
+    
 
+    @IBOutlet weak var Coll: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
