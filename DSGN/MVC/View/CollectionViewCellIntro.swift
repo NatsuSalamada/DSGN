e1fd5b20bba36da461a741134edcdfd75ed55f99
Unknown <natsusalamada@gmail.com>
2018-05-25 15:58:43 +0700

    DATVSHIEULADOIBANTHAN

---
+++ DSGN/MVC/View/CollectionViewCellIntro.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCellIntro.swift
+//  DSGN
+//
+//  Created by 5k on 5/25/18.
+//  Copyright © 2018 NatsuSalamada. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCellIntro: UICollectionViewCell {
+    
+}
